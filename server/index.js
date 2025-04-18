e50d0e2a365382940ecf3252503e3868bd771c3f
Dima <dimakichigin94@gmail.com>
2025-04-18 13:57:58 +0300

    add websocket server with sinus generation

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const WebSocket = require('ws');
+
+const PORT = 8080;
+const serverWs = new WebSocket.Server({ port: PORT });
+
+console.log(`Server started on PORT = ${PORT}`)
+
+serverWs.on('connection', server => {
+	console.log('Connection open')
+	let time = 0;
+	const interval = setInterval(() => {
+		const value = Math.sin(time);
+	    server.send(JSON.stringify({ time, value }));
+	    t += 0.1;
+	}, 10);
+
+	server.on('close', () => {
+		console.log('Connection close')
+	    clearInterval(interval);
+	});
+});
